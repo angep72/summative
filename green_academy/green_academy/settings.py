d2d06575ed6bfa095152da131d4c9dd3759c09e1
angep73 <p.umunyana@alustudent.com>
2025-03-05 10:02:19 +0200

    adding

--- green_academy/green_academy/settings.py
+++ green_academy/green_academy/settings.py
@@ -97,7 +97,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# This is where static files will be collected
 
 # Default auto field for Django models
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
